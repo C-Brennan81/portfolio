bc69a1460f5fb2c209ff0532449475c7b0998f0d
Craig Brennan <craigbrennan81@gmail.com>
2025-03-19 17:16:44 +0000

    Nothing is working

--- ember-vite-portfolio/vite.config.js
+++ ember-vite-portfolio/vite.config.js
@@ -4,9 +4,4 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      'react-router-dom': require.resolve('react-router-dom'),
-    },
-  },
 });
