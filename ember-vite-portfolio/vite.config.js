6ace8fa4c6fd4db8baf0bf430913343e36e9efae
Craig Brennan <craigbrennan81@gmail.com>
2025-03-19 16:48:43 +0000

    Fix react-router-dom build issue

--- ember-vite-portfolio/vite.config.js
+++ ember-vite-portfolio/vite.config.js
@@ -1,6 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-})
+  optimizeDeps: {
+    include: ['react-router-dom']
+  },
+  build: {
+    rollupOptions: {
+      external: ['react-router-dom'] // helps Vite resolve this on Vercel
+    }
+  }
+});
